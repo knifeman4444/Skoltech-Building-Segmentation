58e477d3e7ae57394058aa447988ae26019bdf2a
Pavel Nikulin <pashenca8@mail.ru>
2023-11-24 01:25:55 +0300

    Fix segmentation dataset

--- Utils/segmentation_dataset.py
+++ Utils/segmentation_dataset.py
@@ -2,6 +2,8 @@ import torch
 import cv2
 import os
 
+from sklearn.model_selection import train_test_split
+
 
 def default_aug(image, mask):
     return image, mask
@@ -13,57 +15,25 @@ def default_preprocessing(image):
 
 class SegmentationDataset(torch.utils.data.Dataset):
     def __init__(self,
-                 tile_size,
-                 path_to_pics,
-                 path_to_masks=None,                   # None, если нет лейблов
-                 augmentations=default_aug,            # Аугментации
-                 preprocessing=default_preprocessing   # Обработка данных для модели
+                 tiles,
+                 masks,
+                 augmentations,            # Аугментации
+                 preprocessing   # Обработка данных для модели
                  ):
-        # Загрузка картинок
-        self.images = []
-        self.prefix_sizes = [0]
-        self.tile_size = tile_size
-
-        for root, dirs, files in os.walk(path_to_pics):
-            for file in sorted(files):
-                self.images.append(cv2.imread(os.path.join(root, file)))
-                h, w, _ = self.images[-1].shape
-                tile_count = (h // tile_size) * (w // tile_size)
-                self.prefix_sizes.append(self.prefix_sizes[-1] + tile_count)
-
-        # Загрузка масок
-        self.has_mask = False
-        if path_to_masks is not None:
-            self.has_mask = True
-            self.masks = []
-            for root, dirs, files in os.walk(path_to_masks):
-                for file in sorted(files):
-                    mask = cv2.imread(os.path.join(root, file))
-                    mask = (mask[:, :, 0] > 0).astype('uint8')
-                    self.masks.append(mask)
-
+        self.has_mask = masks is not None
+        self.tiles = tiles
+        self.masks = masks
         self.augmentations = augmentations
         self.preprocessing = preprocessing
 
     def __len__(self):
-        return self.prefix_sizes[-1]
-
-    def crop_tile(self, arr, img_index, tile_index):
-        h, w, _ = self.images[img_index].shape
-        w_tile_count = w // self.tile_size
-        tile_idx_h, tile_idx_w = tile_index // w_tile_count, tile_index % w_tile_count
-        h_start, w_start = self.tile_size * tile_idx_h, self.tile_size * tile_idx_w
-        return arr[img_index][h_start: h_start + self.tile_size, w_start: w_start + self.tile_size]
+        return len(self.tiles)
 
     def get_item(self, idx):
-        img_idx = 0
-        while self.prefix_sizes[img_idx + 1] < idx:
-            img_idx += 1
-        tile_idx = idx - self.prefix_sizes[img_idx]
-        image = self.crop_tile(self.images, img_idx, tile_idx)
+        image = self.tiles[idx]
         mask = None
         if self.has_mask:
-            mask = self.crop_tile(self.masks, img_idx, tile_idx)
+            mask = self.masks[idx]
         image, mask = self.augmentations(image, mask)
         if self.has_mask:
             mask = torch.from_numpy(mask).float()
@@ -77,3 +47,52 @@ class SegmentationDataset(torch.utils.data.Dataset):
         if self.has_mask:
             return image, mask
         return image
+
+
+def get_tiles(tile_size,
+              path_to_pics,
+              path_to_masks=None,                   # None, если нет лейблов
+              ):
+    images = []
+    masks = None
+
+    for root, dirs, files in os.walk(path_to_pics):
+        for file in sorted(files):
+            img = cv2.imread(os.path.join(root, file))
+            h, w, _ = img.shape
+
+            for h_coord in range(0, h // tile_size):
+                for w_coord in range(0, w // tile_size):
+                    y = h_coord * tile_size
+                    x = w_coord * tile_size
+                    images.append(img[y: y + tile_size, x: x + tile_size])
+
+    # Загрузка масок
+    if path_to_masks is not None:
+        masks = []
+        for root, dirs, files in os.walk(path_to_masks):
+            for file in sorted(files):
+                mask = cv2.imread(os.path.join(root, file))
+                mask = (mask[:, :, 0] > 0).astype('uint8')
+                h, w = mask.shape
+
+                for h_coord in range(0, h // tile_size):
+                    for w_coord in range(0, w // tile_size):
+                        y = h_coord * tile_size
+                        x = w_coord * tile_size
+                        masks.append(mask[y: y + tile_size, x: x + tile_size])
+
+    return images, masks
+
+
+def get_datasets(tile_size,
+                 path_to_pics,
+                 path_to_masks=None,
+                 augmentations=default_aug,            # Аугментации
+                 preprocessing=default_preprocessing   # Обработка данных для модели
+                 ):
+    tiles, masks = get_tiles(tile_size, path_to_pics, path_to_masks)
+    train_tiles, val_tiles, train_masks, val_masks = train_test_split(tiles, masks, test_size=0.2, random_state=42)
+    train_dataset = SegmentationDataset(train_tiles, train_masks, augmentations, preprocessing)
+    val_dataset = SegmentationDataset(val_tiles, val_masks, default_aug, preprocessing)
+    return train_dataset, val_dataset
